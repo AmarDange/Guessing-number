e0bd5455d232da5794fb075243efe312531333dd
AmarDange <amardange218@gmail.com>
2023-03-16 19:18:11 +0000

    add code successfully

--- run.py
+++ run.py
@@ -2,7 +2,26 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
+import pyfiglet
+from termcolor import colored
 
+
+def Welcome():
+    """
+    Function to display home page
+    """
+    title = "Welcome  to  NUMBER  GUESSING  GAME "
+    print(colored(pyfiglet.figlet_format(title), "green"))
+
+Welcome()
+name = input(" Please Enter your name :  ")
+print("Welcome", name, "to this 'Number Guessing Game'")
+playing = input("Do you want to play?")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's start to  play game ! :")
 top_of_range = input("Type a number:  ")
 
 if top_of_range.isdigit():
@@ -16,6 +35,7 @@ else:
     quit()
 
 random_number = random.randint(0, 11)
+guesses = 0
 
 while True:
     guesses += 1
@@ -28,9 +48,10 @@ while True:
     
     if user_guess == random_number:
         print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
     else:
         print("You got it wrong!")
 
 print("You got it in", guesses, "guesses")
-
-    
