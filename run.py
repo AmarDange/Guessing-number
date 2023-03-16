38560da190850c688fa9ec666f5cbb120aeaaf66
AmarDange <amardange218@gmail.com>
2023-03-16 06:33:44 +0000

    Add user guess

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+top_of_range = input("Type a number:  ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print('Please type a number larger than 0 next time.')
+        quit()
+else:
+    print('Please type a number next time.')
+    quit()
+
+random_number = random.randint(0, 11)
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+    
+    if user_guess == random_number:
+        print("You got it!")
+    else:
+        print("You got it wrong!")
+
+print("You got it in", guesses, "guesses")
+
+    
